714b041762ca296814c20c82451108bc7cb25a2c
Xie Linxi@Dongguan University of Technology <nanhean@outlook.com>
2023-04-12 18:29:07 +0800

    lagou_search

---
+++ The_first_day.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestLagouSearch():
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_lagouSearch(self):
+        self.driver.get("https://www.lagou.com/")
+        self.driver.set_window_size(1361, 730)
+        self.driver.find_element(By.ID, "cboxClose").click()
+        self.driver.implicitly_wait(3)
+        self.driver.find_element(By.ID, "search_input").click()
+        self.driver.find_element(By.ID, "search_input").send_keys("软件测试")
+        self.driver.find_element(By.ID, "search_button").click()
+        self.driver.find_element(By.CLASS_NAME, "active__YmPJe").click()  # 点击“最新”
+
+        res_element = self.driver.find_element(By.ID, "jobList")
+        assert '测试' in res_element.text
